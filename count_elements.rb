e20fdd5b4fcbe4506c8bc7929a17d6947e8d131e
KellyStamps <kellydsample@gmail.com>
2017-11-20 22:50:23 -0500

    took out example

--- count_elements.rb
+++ count_elements.rb
@@ -11,7 +11,3 @@ def count_elements(array)
   end
   new_hash
 end
-
-animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
-count_elements(animals)
-# => {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}

a494b1c6bc98ee8d65194a349067334f6f874003
KellyStamps <kellydsample@gmail.com>
2017-11-20 22:47:17 -0500

    wrote count_elements method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+
+  array.each do |animal|
+    if new_hash[animal]
+      new_hash[animal] += 1
+    else
+      new_hash[animal] = 1
+    end
+  end
+  new_hash
 end
- 
+
+animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+count_elements(animals)
+# => {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
